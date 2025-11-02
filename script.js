0ed866e0ec32393db8eac911a42d7dcbbf549818
Himanshu Sharma <143724283+himanshu7437@users.noreply.github.com>
2025-11-02 07:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 function convertToRoman(num) {
-    const obj = [
-        ['M', 1000], 
-        ['CM', 900],
-        ['D', 500], 
-        ['CD', 400],
-        ['C', 100], 
-        ['XC', 90],
-        ['L', 50], 
-        ['XL', 40],
-        ['X', 10], 
-        ['IX', 9],
-        ['V', 5], 
-        ['IV', 4],
-        ['I', 1]
-    ];
+  const obj = {
+    0: ['M', 1000], 
+    1: ['D', 500], 
+    2: ['C', 100], 
+    3: ['L', 50], 
+    4: ['X', 10], 
+    5: ['V', 5], 
+    6: ['I', 1]
+  };
 
-    let roman = '';
+  let result = '';
 
-    for (let i = 0; i < obj.length; i++) {
-        const [letter, value] = obj[i];
-        while (num >= value) {
-            roman += letter;
-            num -= value;
-        }
+  for (let i = 0; i <= 6; i++) {
+    let [roman, value] = obj[i];
+
+    // handle 4s (like IV, XL, CD)
+    if (i % 2 === 0 && num >= value - obj[i + 2]?.[1]) {
+      result += obj[i + 2][0] + roman;
+      num -= value - obj[i + 2][1];
     }
 
-    return roman;
+    // handle 9s (like IX, XC, CM)
+    if (i % 2 === 0 && num >= value - obj[i + 1]?.[1] && i < 6) {
+      result += obj[i + 2][0] + roman; 
+      num -= value - obj[i + 2][1];
+    }
+
+    // append as many as possible
+    while (num >= value) {
+      result += roman;
+      num -= value;
+    }
+  }
+
+  return result;
 }
 
-// Test it
-console.log(convertToRoman(36)); // Output: XXXVI
+console.log(convertToRoman(36)); // XXXVI
+console.log(convertToRoman(944)); // CMXLIV
